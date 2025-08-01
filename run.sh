a875a6ad5472f1b03d70965f19cb8d498a12d7b9
a43992899 <a43992899@gmail.com>
2025-08-01 14:41:47 +0800

    speed up train

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 bash scripts/probe.MuQ.MTGGenre.sh
-bash scripts/probe.MuQ.MTGInstrument.sh
-bash scripts/probe.MuQ.MTGMood.sh
+# bash scripts/probe.MuQ.MTGInstrument.sh
+# bash scripts/probe.MuQ.MTGMood.sh
 bash scripts/probe.MuQ.MTGTop50.sh
 
 bash scripts/probe.MERT-v1-95M.MTGGenre.sh
