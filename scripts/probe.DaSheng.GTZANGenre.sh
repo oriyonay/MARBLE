fcaece1523f5c9041ad0fed3cd3415fa5c73ad3d
a43992899 <a43992899@gmail.com>
2025-08-04 21:16:10 +0800

    fix dasheng

---
+++ scripts/probe.DaSheng.GTZANGenre.sh
@@ -0,0 +1,3 @@
+rm -rf output/probe.GTZANGenre.DaSheng
+python cli.py fit -c configs/probe.DaSheng.GTZANGenre.yaml
+python cli.py test -c configs/probe.DaSheng.GTZANGenre.yaml
--- scripts/probe.DaSheng.MTGGenre.sh
+++
@@ -1,3 +0,0 @@
-rm -rf output/probe.MTGGenre.DaSheng
-torchrun --nproc_per_node=4  cli.py fit -c configs/probe.DaSheng.MTGGenre.yaml
-torchrun --nproc_per_node=4  cli.py test -c configs/probe.DaSheng.MTGGenre.yaml
